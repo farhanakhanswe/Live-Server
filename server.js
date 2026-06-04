87465fef9621c5691515d42a103c809246d1bbca
Farhana <farhanakhanswe@gmail.com>
2026-06-04 23:47:47 +0600

    feat: serve static html files

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require("http");
+const fs = require("fs");
+const port = 3000;
 
 const server = http.createServer((request, response) => {
-    response.end("Hello");
+    if(request.url === "/"){
+       fs.readFile("public/index.html", (error, data) => {
+            response.end(data);
+       });
+    } 
 });
 
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
