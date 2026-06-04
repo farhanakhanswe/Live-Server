a7f1b6ae9914753f0afbebdce5b55ec2d72c0ccc
Farhana <farhanakhanswe@gmail.com>
2026-06-05 00:31:47 +0600

    feat: serve requested files from public directory

--- server.js
+++ server.js
@@ -4,11 +4,23 @@ const port = 3000;
 
 const server = http.createServer((request, response) => {
     if(request.url === "/"){
-       fs.readFile("public/index.html", (error, data) => {
-            response.end(data);
-       });
+        serveFile("/index.html", response);
+    }else{
+        serveFile(request.url, response);
     } 
 });
+
+const serveFile = (filename, response) => {
+    fs.readFile(`public${filename}`, (error, data) => {
+        if(!error){
+            response.end(data); 
+        }else{
+            response.statusCode = 404;
+            response.end("File not found!");
+        }
+    });
+}
+
 
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
