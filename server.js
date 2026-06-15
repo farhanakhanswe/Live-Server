efe7093a18f9659095cdd8a0345c4ef3ce20c63e
Farhana <farhanakhanswe@gmail.com>
2026-06-15 22:24:12 +0600

    refactor: move logic for router and file serving to separate modules

--- server.js
+++ server.js
@@ -1,87 +1,13 @@
 const http = require("http");
-const fs = require("fs");
 const config = require("./config.json");
-const path = require("path");
-const mimeTypes = require("./mime-types.json");
-const fileReloadState = require("./fileReloadState");
+
 const { watchFiles } = require("./fileWatcher");
-
-const CONTENT_TYPE = "Content-Type";
-const NOT_FOUND_404 = 404;
-
-const htmlExtension = ".html";
-const jsonExtension = ".json";
-const checkIfFileChangedAPI = "/check-file-changes";
+const { routeRequest } = require("./router");
 
 // functions start here
 
-const setContentHeader = (ext, response) => {
-    const type = mimeTypes[ext] || "application/octet-stream";
-    response.setHeader(CONTENT_TYPE, type);
-}
-
-const serve404NotFound = (response) => {
-    response.statusCode = NOT_FOUND_404;
-    response.setHeader(CONTENT_TYPE, mimeTypes[htmlExtension]);
-
-    fs.readFile(`public/404.html`, (error, data) => {
-        response.end(data);
-    });
-}
-
-const serveFile = (filename, response) => {
-    fs.readFile(`public${filename}`, (error, data) => {
-        if(!error){
-            response.end(data); 
-        }else{
-            serve404NotFound(response);
-        }
-    });
-}
-
-
-const isSafePath = (requestPath) => {
-    const publicDir = path.resolve("public");
-    const requestedFile = path.resolve("public", "." + requestPath);
-    console.log("publicDir" + publicDir);
-    console.log(`requested file ${requestedFile}`);
-
-    return requestedFile.startsWith(publicDir);
-}
-
 const server = http.createServer((request, response) => {
-    console.log(request.url);
-    console.log("ext" + path.extname(request.url));
-
-    if(request.url === checkIfFileChangedAPI){
-        response.setHeader(CONTENT_TYPE, mimeTypes[jsonExtension]);
-
-        response.end(JSON.stringify({
-            changed: fileReloadState.getHasFileChanged()
-        }));
-
-        fileReloadState.setHasFileChanged(false);
-        return;
-    }else if(request.url === "/"){
-        setContentHeader(htmlExtension, response);
-        serveFile("/index.html", response);
-    }else if(path.extname(request.url) !== ""){
-
-        console.log("before");
-        if (!isSafePath(request.url)) {
-            console.log("during");
-            serve404NotFound(response);
-            return;
-        }
-
-        console.log("after");
-        const fileFormat = path.extname(request.url);
-        setContentHeader(fileFormat, response);
-        serveFile(request.url, response);
-    }else{
-        serve404NotFound(response); 
-    }
-
+    routeRequest(request, response);
 });
 
 server.listen(config.port, () => {
