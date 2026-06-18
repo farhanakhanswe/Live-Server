a74a875ea4964587b694248aa2fcfa0fb6619dfa
Farhana <farhanakhanswe@gmail.com>
2026-06-18 22:35:31 +0600

    feat: watch nested directories recursively

--- fileWatcher.js
+++ fileWatcher.js
@@ -1,14 +1,32 @@
 const fs = require("fs");
+const path = require("path");
 const fileReloadState = require("./fileReloadState");
 
-const watchFiles = () => {
-    console.log("watching files");
-    fs.watch("public", (eventType, filename) => {
-        console.log(eventType, filename);
+const watchDirectory = (directoryPath) => {
+    console.log(`watching directory path: ${directoryPath}` );
+
+    fs.watch(directoryPath, (eventType, filename) => {
+        console.log(`eventType: ${eventType} and fileName: ${filename}`);
         if(eventType === "change"){
             fileReloadState.setHasFileChanged(true);
         }
     });
+}
+
+const watchDirectoriesRecursively = (directoryPath) => {
+    watchDirectory(directoryPath);
+
+    const items = fs.readdirSync(directoryPath, { withFileTypes: true});
+    for(const item of items){
+        if(item.isDirectory()){
+            const childDirectory = path.join(directoryPath, item.name);
+            watchDirectoriesRecursively(childDirectory);
+        }
+    }
+}
+
+const watchFiles = () => {
+    watchDirectoriesRecursively("public");
 }
 
 module.exports = {
