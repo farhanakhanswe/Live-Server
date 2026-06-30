edcc21b582535b67e1a94de1d320022b42c28ccc
Farhana <farhanakhanswe@gmail.com>
2026-06-30 23:49:02 +0600

    feat: send HTML's JS and CSS dependency data to server

---
+++ public/postHTMLTagData.js
@@ -0,0 +1,56 @@
+const browserUrl = window.location.href;
+const htmlDependenciesMappingAPI = "/v1/html-dependencies-mapping";
+const links =  document.getElementsByTagName("link");
+const scripts = document.getElementsByTagName("script");
+const cssLinks = [];
+const scriptLinks = [];
+const cssExt = ".css";
+const jsExt = ".js";
+
+for(let i = 0; i < links.length ; i++){
+    const fileName = links[i].href.split('/').pop().toLowerCase();
+    if(fileName.endsWith(cssExt)){
+        cssLinks.push(links[i].href)
+    }
+}
+
+for(let i = 0; i < scripts.length ; i++){
+    const fileName = scripts[i].src.split('/').pop().toLowerCase();
+    if(fileName.endsWith(jsExt)){
+        scriptLinks.push(scripts[i].src)
+    }
+}
+
+const payload = {
+    "browserUrl": browserUrl,
+    "links": cssLinks,
+    "scripts": scriptLinks
+}
+
+console.log(payload);
+
+async function postHTMLTagData(){
+    try{
+        const response = await fetch(htmlDependenciesMappingAPI, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        });
+
+        if(!response.ok){
+            throw new Error(`Error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log("Success: ", result );
+    }catch(error){
+        console.log("Error: ", error);
+    }
+}
+
+setInterval(postHTMLTagData, 2000);
+
+
+
